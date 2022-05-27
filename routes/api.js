34feecc4bab3c83f5b35d4fd5829ea56a1578e9e
Rachata Rongluan <78256283+Rachata9107@users.noreply.github.com>
2022-05-27 14:26:15 +0700

    Update api.js

--- routes/api.js
+++ routes/api.js
@@ -5,8 +5,8 @@ router.post('/number', (req, res) => {
     const { nums, target } = req.body
     function getIndex(arr, target) {
         for (let i = 0; i < arr.length; i++) {
-            for (let j = 0; j < arr.length; j++) {
-                if (arr[i] + arr[j] === target && i !== j) return [i, j]
+            for (let j = i+1; j < arr.length; j++) {
+                if (arr[i] + arr[j] === target) return [i, j]
             }
         }
         return []
